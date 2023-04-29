4042faae9e84d5b8993d8ea1af9e711a01bb7d30
swtabhimanyu <swt.abhimanyu.31@gmail.com>
2023-04-29 19:27:18 +0200

    First commit

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	
+//	Logger logger=Logger.getLogger(DemoController.class);
+
+	@RequestMapping("/")
+	public String getHelloWorld() {
+		System.out.println("Hello Printed");
+		return "Hello World";
+	}
+}
