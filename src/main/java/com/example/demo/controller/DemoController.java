e44a8d99777acf3aed6d753e04e4fa183f51297d
Abhimanyu Saraswat <swt.abhimanyu.31@gmail.com>
2024-02-13 22:40:41 +0100

    Try one more thing

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
-	
+
 //	Logger logger=Logger.getLogger(DemoController.class);
 
 	@RequestMapping("/")
